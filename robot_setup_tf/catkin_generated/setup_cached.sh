c062c5183b18c55a2b4da2ea5fdfea1d2b63e1fd
balestrieri.gepp@gmail.com <balestrieri.gepp@gmail.com>
2015-10-15 19:36:46 +0200

    Added robot setup tf scropt

---
+++ robot_setup_tf/catkin_generated/setup_cached.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/userk/Development/Ros/robot_setup_tf/test_results"
+export ROS_TEST_RESULTS_DIR="/home/userk/Development/Ros/robot_setup_tf/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/userk/Development/Ros/robot_setup_tf/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/userk/Development/Ros/robot_setup_tf/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/userk/Development/Ros/robot_setup_tf/devel/lib:/home/userk/Development/Ros/robot_setup_tf/devel/lib/i386-linux-gnu:/opt/ros/indigo/lib/i386-linux-gnu:/opt/ros/indigo/lib"
+export PATH="/home/userk/Development/Ros/robot_setup_tf/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/userk/Development/Ros/robot_setup_tf/devel/lib/pkgconfig:/home/userk/Development/Ros/robot_setup_tf/devel/lib/i386-linux-gnu/pkgconfig:/opt/ros/indigo/lib/i386-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig:/usr/local/lib/pkgconfig"
+export PYTHONPATH="/home/userk/Development/Ros/robot_setup_tf/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/userk/Development/Ros/robot_setup_tf/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/userk/Development/Ros/robot_setup_tf:$ROS_PACKAGE_PATH"
