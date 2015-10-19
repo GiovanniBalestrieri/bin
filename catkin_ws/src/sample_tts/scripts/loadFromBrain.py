bb382a877b0f28d5cdd4497a9684780054f3d8a6
balestrieri.gepp@gmail.com <balestrieri.gepp@gmail.com>
2015-10-20 01:52:35 +0200

    Added pazienza file fro bread

--- catkin_ws/src/sample_tts/scripts/loadFromBrain.py
+++ catkin_ws/src/sample_tts/scripts/loadFromBrain.py
@@ -9,14 +9,14 @@ sakura.setBotPredicate("name","Sakura")
 sakura.setBotPredicate("hometown","170.01.5.5")
 sakura.setBotPredicate('master', 'gepp')
 
-if os.path.isfile("noise0001.brn"):
-	sakura.bootstrap(brainFile = "noise0001.brn")
-else:
+#if os.path.isfile("noise0001.brn"):
+#	sakura.bootstrap(brainFile = "noise0001.brn")
+#else:
 #load all AIML files and save a new brain
-	sakura.bootstrap(learnFiles = "startup.xml", commands = "load aiml b")
-	sakura.saveBrain("noise0001.brn")
+#	sakura.bootstrap(learnFiles = "startup.xml", commands = "load aiml b")
+#	sakura.saveBrain("noise0001.brn")
 	
-#sakura.learn('startup.xml')
+sakura.learn("pazienza.aiml")
 #sakura.respond('load aiml b')
 #Saving loaded patterns into a brain file
 #sakura.saveBrain('standard.brn')
