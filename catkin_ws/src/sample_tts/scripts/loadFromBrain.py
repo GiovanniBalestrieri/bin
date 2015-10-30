e39a11e38a046ecfb1d137b1cf06ebae5eaddc1c
balestrieri.gepp@gmail.com <balestrieri.gepp@gmail.com>
2015-10-30 01:00:22 +0100

    updare pazienza, loadfrom brain

--- catkin_ws/src/sample_tts/scripts/loadFromBrain.py
+++ catkin_ws/src/sample_tts/scripts/loadFromBrain.py
@@ -9,20 +9,23 @@ sakura.setBotPredicate("name","Sakura")
 sakura.setBotPredicate("hometown","170.01.5.5")
 sakura.setBotPredicate('master', 'gepp')
 
-#if os.path.isfile("noise0001.brn"):
-#	sakura.bootstrap(brainFile = "noise0001.brn")
-#else:
-#load all AIML files and save a new brain
-#	sakura.bootstrap(learnFiles = "startup.xml", commands = "load aiml b")
-#	sakura.saveBrain("noise0001.brn")
+if os.path.isfile("noise0001.brn"):
+	sakura.bootstrap(brainFile = "noise0001.brn")
+else:
+	#load all AIML files and save a new brain
+	sakura.bootstrap(learnFiles = "startup.xml", commands = "load aiml b")
+	sakura.saveBrain("noise0001.brn")
 	
-sakura.learn("pazienza.aiml")
+#sakura.bootstrap(learnFiles = "startup.xml", commands = "load aiml b")
+#sakura.learn("pazienza.aiml")
 #sakura.respond('load aiml b')
 #Saving loaded patterns into a brain file
 #sakura.saveBrain('standard.brn')
 while True:
 	message = raw_input(">: ")
 	if message == "quit":
+			exit()
+	if message == "exit":
 			exit()
 	#else message == "save":
 	#	sakura.saveBrain("noise0001.brn")
